fe1480daa45fb505f9407747bf305e10d03e1fb7
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-06-30 15:23:59 -0300

    Fazendo dowload das capas em '.svg'
    baixando as imagens dos livros em '.svg' e alterando o html| modificando
    o estilo do carrossel no 'carrossel.css'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,29 @@
-import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js';
-
 const swiper = new Swiper('.swiper', {
-    direction: 'vertical',
+    spaceBetween: 10,
+    slidePerView: 1,
+    direction: 'horizontal',
     loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10 
+        },
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
