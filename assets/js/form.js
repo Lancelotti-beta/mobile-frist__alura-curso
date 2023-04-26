b3f61100d41f7eecf1b2df06aba968a7739a5f34
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2023-04-26 20:53:13 -0300

    adicionando function ao script form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,30 @@
+const $cepInput = document.querySelector('input[name="cep"]');
+
 const consultaCEP = async (userCEP) => {
-	await fetch(`https://viacep.com.br/ws/${userCEP}/json/`)
-	.then(resposta => {
-		console.log(resposta)
-	})
+    await fetch(`https://viacep.com.br/ws/${userCEP}/json/`)
+        .then(resposta => resposta.json())
+	.then(resultado => {
+		try {
+		    console.log(resultado);
+		    if(resultado.erro)
+			throw Error("Endereço invalido ou não encontrado!");
+		    return resultado;
+		} catch(erro) {
+		    console.log(erro);
+		}
+	});
 
 }
 
+$cepInput.addEventListener('blur', async (input) => {
+	console.log(input.target.value);
+	const resposta = await consultaCEP(input.target.value);
+	preencheFromulario(resposta);
+});
+
+function preeincheFormulario({bairro, localidade, logadouro, uf}) {
+    document.querySelector('input[name="endereco"]').value = logadouro;
+    document.querySelector('input[name="bairro"]').value = bairro;
+    document.querySelector('input[name="cidade"]').value = localidade;
+}
 
