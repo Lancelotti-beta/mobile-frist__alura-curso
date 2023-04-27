e9eb2a136101f019ca3c54b32f414b38ad8624dd
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2023-04-27 16:54:53 -0300

    Update form.js
    corrigindo form.js do merge

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,5 @@
 const $cepInput = document.querySelector('input[name="cep"]');
 
-<<<<<<< HEAD
 async function consultaCEP(cepDoUsuario) {
 	// const erro = document.querySelector("#error");
 	// erro.innerHTML = "";
@@ -17,26 +16,9 @@ async function consultaCEP(cepDoUsuario) {
 		//erro.innerHTML = `<h2>${error}</h2>`;
 		console.log(error);
 	}
-=======
-const consultaCEP = async (userCEP) => {
-    await fetch(`https://viacep.com.br/ws/${userCEP}/json/`)
-        .then(resposta => resposta.json())
-	.then(resultado => {
-		try {
-		    console.log(resultado);
-		    if(resultado.erro)
-			throw Error("Endereço invalido ou não encontrado!");
-		    return resultado;
-		} catch(erro) {
-		    console.log(erro);
-		}
-	});
->>>>>>> b3f61100d41f7eecf1b2df06aba968a7739a5f34
-
 }
 
 $cepInput.addEventListener('blur', async (input) => {
-<<<<<<< HEAD
 	const resposta = await consultaCEP(input.target.value);
 	preencheFormulario(resposta);
 });
@@ -45,17 +27,7 @@ function preencheFormulario({bairro, localidade, logradouro, uf}) {
     document.querySelector('input[name="endereco"]').value = logradouro;
     document.querySelector('input[name="bairro"]').value = bairro;
     document.querySelector('input[name="cidade"]').value = localidade;
-	document.querySelector('select[name="estado"]').value = uf;
-=======
-	console.log(input.target.value);
-	const resposta = await consultaCEP(input.target.value);
-	preencheFromulario(resposta);
-});
+    document.querySelector('select[name="estado"]').value = uf;
 
-function preeincheFormulario({bairro, localidade, logadouro, uf}) {
-    document.querySelector('input[name="endereco"]').value = logadouro;
-    document.querySelector('input[name="bairro"]').value = bairro;
-    document.querySelector('input[name="cidade"]').value = localidade;
->>>>>>> b3f61100d41f7eecf1b2df06aba968a7739a5f34
 }
 
